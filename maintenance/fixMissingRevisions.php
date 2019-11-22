331f1be5f641fad7ec616fa4e2bfde30ec71a9e8
Michael Chaudhary <3246015-chaudharymic@users.noreply.gitlab.com>
2019-11-22 18:48:49 +0000

    Add script to fix missing revisions

---
+++ maintenance/fixMissingRevisions.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Curse Inc.
+ * Hydra Core
+ * Fix missing revision_text
+ *
+ * @copyright (c) 2019 Curse Inc.
+ * @license   GPL-2.0-or-later
+ * @package   Hydra Core
+ * @link      https://gitlab.com/hydrawiki
+ */
+
+require_once dirname(__DIR__, 3) . '/maintenance/Maintenance.php';
+class FixMissingRevisions extends Maintenance {
+	/**
+	 * Main Constructor
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Fix rev_text_id pointing to non-existent entries";
+	}
+
+	/**
+	 * Find and fix missing revision text.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function execute() {
+		$dbw = wfGetDB(DB_MASTER);
+		$db = wfGetDB(DB_REPLICA);
+		$results = $db->select(['revision'], ['rev_id', 'rev_text_id'], ['rev_text_id not in (select text.old_id from text)'], __METHOD__);
+
+		while ($row = $results->fetchRow()) {
+			$oldID = $row['rev_text_id'];
+			$this->output('Fixing revision ' . $row['rev_id'] . ', which was missing rev_text_id: ' . $row['rev_text_id'] . "\n");
+			$dbw->insert('text', ['old_id' => $row['rev_text_id'], 'old_text' => '', 'old_flags' => 'utf-8'], __METHOD__);
+		}
+	}
+}
+$maintClass = "FixMissingRevisions";
+require_once RUN_MAINTENANCE_IF_MAIN;
