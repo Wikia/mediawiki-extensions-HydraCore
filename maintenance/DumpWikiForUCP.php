f932fc0b121d1806331d3e752de270d6afbd4746
Samuel Hilson <samrhilson@gmail.com>
2020-08-07 15:46:50 -0500

    Adding maintenance script to export a db dump

---
+++ maintenance/DumpWikiForUCP.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Fandom Inc.
+ * HydraAuth
+ * Handle setting read-only, waiting for slaves, and dumping the wiki
+ *
+ * @package   HydraCore
+ * @author    Samuel Hilson
+ * @copyright (c) 2020 Fandom Inc.
+ * @license   GPL-2.0-or-later
+ * @link      https://gitlab.com/hydrawiki
+ */
+
+use DynamicSettings\Sites;
+use DynamicSettings\Wiki;
+use MediaWiki\Shell\Shell;
+
+require_once dirname(__DIR__, 3) . '/maintenance/Maintenance.php';
+
+class DumpWikiForUCP extends Maintenance {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription('Handle setting read-only, waiting for slaves, and dumping the wiki');
+		$this->requireExtension('DynamicSettings');
+		$this->DIR = dirname(__DIR__) . '/maintenance';
+		$this->addOption('domain', 'The wiki to run against', true, true);
+	}
+
+	/**
+	 * Perform wiki export
+	 *
+	 * @return void
+	 */
+	public function execute() {
+		$domain = $this->getOption('domain', null);
+		$wiki = Wiki::loadFromDomain($domain);
+		$this->enableMaintenance($wiki);
+		// get the DB host name, user and password to be used by mysqldump
+		$info = $wiki->getDatabase();
+		$command = Shell::command([
+			$this->DIR . "/exportDump.sh",
+		])
+			->params("-h{$info["db_server"]}")
+			->params("-u{$info["db_user"]}")
+			->params("-p{$info["db_password"]}")
+			->params("-d{$info["db_name"]}")
+			->limits([
+				'time' => 0,
+				'memory' => 0,
+				'filesize' => 0
+			]);
+
+		$result = $command->execute();
+		if ($result->getExitCode() !== 0) {
+			$this->error($result->getStderr() . "\n");
+			throw new Exception("Unable to generate a SQL dump of '{$domain}' (using {$info['db_server']})");
+		}
+		$this->output($result->getStdout());
+		return true;
+	}
+
+	/**
+	 * Add read only setting
+	 *
+	 * @param Wiki $wiki
+	 *
+	 * @return void
+	 */
+	private function enableMaintenance(Wiki $wiki) {
+		$domain	= $wiki->getDomains()->getDomain();
+		$settings = file_get_contents(dirname(__DIR__, 3) . '/sites/' . $domain . '/LocalSettings.php');
+		if (strpos($settings, '$wgReadOnly') === false) {
+			$settings .= '$wgReadOnly = \'This Wiki is being migrated to UCP.\';' . "\n";
+			Sites::writeSiteFile($domain, 'LocalSettings.php', $settings);
+		}
+		sleep(10);
+	}
+}
+
+$maintClass = DumpWikiForUCP::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ maintenance/exportDump.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while getopts ":h:u:p:d:" opt; do
+  case $opt in
+    h) host="$OPTARG"
+    ;;
+    u) user="$OPTARG"
+    ;;
+    p) pass="$OPTARG"
+    ;;
+    d) db="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+mysqldump -u$user -p$pass -h$host --ignore-table=$db.user --ignore-table=$db.user_global $db
