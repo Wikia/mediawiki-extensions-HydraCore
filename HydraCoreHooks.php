afce54c0c69b46bed895d833807921c386eb0ffe
Michał Turek-Brzozowski <mturek@fandom.com>
2023-03-13 15:20:39 +0100

    PLATFORM-7959 Remove 'withads' param that's been broken for a while now

--- HydraCoreHooks.php
+++ HydraCoreHooks.php
@@ -1,9 +1,6 @@
 <?php
 
-use MediaWiki\Api\Hook\APIAfterExecuteHook;
-use MediaWiki\Api\Hook\APIGetAllowedParamsHook;
 use MediaWiki\Api\Hook\APIGetDescriptionMessagesHook;
-use MediaWiki\Api\Hook\APIGetParamDescriptionMessagesHook;
 use MediaWiki\Hook\ParserFirstCallInitHook;
 
 /**
@@ -19,30 +16,19 @@ use MediaWiki\Hook\ParserFirstCallInitHook;
  *
  */
 class HydraCoreHooks implements
-	APIGetAllowedParamsHook,
 	APIGetDescriptionMessagesHook,
-	APIGetParamDescriptionMessagesHook,
-	ParserFirstCallInitHook,
-	APIAfterExecuteHook
+	ParserFirstCallInitHook
 {
 
-	public function __construct(private HydraCore $core) {
+	public function __construct( private HydraCore $core ) {
 	}
 
 	/**
 	 * Force X-Mobile header.
 	 */
-	public static function onBeforePageDisplayMobile( $output, $skin ): void {
+	public function onBeforePageDisplayMobile( $output, $skin ): void {
 		$response = $output->getRequest()->response();
 		$response->header( "X-Mobile: true" );
-	}
-
-	/**
-	 * Add hooks late so that they are ensured to come last.
-	 */
-	public static function addLateHooks(): void {
-		global $wgHooks;
-		$wgHooks['APIAfterExecute'][] = 'HydraCoreHooks::onAPIAfterExecute';
 	}
 
 	/**
@@ -50,32 +36,6 @@ class HydraCoreHooks implements
 	 */
 	public function onParserFirstCallInit( $parser ): void {
 		$parser->setFunctionHook( 'numberofcontributors', [$this->core, 'numberOfContributors'] );
-	}
-
-	/**
-	 * APIGetAllowedParams hook handler
-	 *
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/APIGetAllowedParams
-	 * @param ApiBase &$module
-	 * @param array|bool &$params
-	 */
-	public function onAPIGetAllowedParams( $module, &$params, $flags ): void {
-		if ( $module->getModuleName() == 'parse' ) {
-			$params['withads'] = false;
-		}
-	}
-
-	/**
-	 * APIGetParamDescriptionMessages hook handler
-	 *
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/APIGetParamDescriptionMessages
-	 * @param ApiBase $module
-	 * @param array|bool &$msgs
-	 */
-	public function onAPIGetParamDescriptionMessages( $module, &$msgs ): void {
-		if ( $module->getModuleName() == 'parse' ) {
-			$msgs['withads'] = [ $module->msg( 'api-parse-withads-desc' ) ];
-		}
 	}
 
 	/**
@@ -88,41 +48,6 @@ class HydraCoreHooks implements
 	public function onAPIGetDescriptionMessages( $module, &$msgs ): void {
 		if ( $module->getModuleName() == 'parse' ) {
 			$msgs[] = $module->msg( 'api-parse-modified-hydracore' );
-		}
-	}
-
-	/**
-	 * APIAfterExecute hook handler
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/
-	 * @param ApiBase &$module
-	 * @throws ApiUsageException
-	 */
-	public function onAPIAfterExecute( $module ): void {
-		if ( $module->getModuleName() == 'parse' ) {
-			$data = $module->getResult()->getResultData();
-			$params = $module->extractRequestParams();
-			if ( isset( $data['parse']['text'] ) && $params['withads'] ) {
-				$result = $module->getResult();
-				$result->reset();
-
-				$text = $data['parse']['text'];
-				if ( is_array( $text ) ) {
-					if ( defined( 'ApiResult::META_CONTENT' ) &&
-						 isset( $text[ApiResult::META_CONTENT] )
-					) {
-						$contentKey = $text[ApiResult::META_CONTENT];
-					} else {
-						$contentKey = '*';
-					}
-					$text = $text[$contentKey];
-				}
-
-				$data['parse']['text'] =
-					'<div id="mobileatfmrec">' . HydraHooks::getAdBySlot( 'mobileatfmrec' ) . '</div>' . $text .
-					'<div id="mobilebtfmrec">' . HydraHooks::getAdBySlot( 'mobilebtfmrec' ) . '</div>';
-
-				$result->addValue( null, $module->getModuleName(), $data['parse'] );
-			}
 		}
 	}
 }
